b5bbab30310c760c1121895a0e74b8e86c20c34a
Tsumugii24 <2792474059@qq.com>
2026-03-16 02:02:16 +0800

    update gitignore file

--- debug/stall_repro/wsl_time_single_rl3_48.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd /mnt/d/pocker/solver
-/usr/bin/time -v /mnt/d/pocker/solver/install/console_solver -i /mnt/d/pocker/solver/debug/stall_repro/configs/0048_AcKc2d.txt -r /mnt/d/pocker/solver/resources -m holdem > /tmp/0048_rl3.out 2> /tmp/0048_rl3.time
-grep -E 'Original TexasSolver heuristic|Calibrated peak estimate|Practical peak estimate|Likely peak while solving|Persistent lower bound' /tmp/0048_rl3.out
-grep 'Maximum resident set size' /tmp/0048_rl3.time
